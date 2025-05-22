d896cc6d36ff0e38b566cbaa3deca53aed9d857a
strattonandrew <73186396+strattonandrew@users.noreply.github.com>
2025-05-22 05:09:34 +0000

    Deploying to gh-pages from @ strattonandrew/strattonandrew.github.io@52e749d156a0fd19388e4fa4b6ba6c6ca7b01111 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -417,22 +417,7 @@ ninja.data = [{
           section: "News",},{id: "news-i-m-presenting-my-ra-l-paper-and-have-two-workshop-papers-long-horizon-motion-prediction-and-advances-in-social-navigation-at-icra-2025-come-say-hello-at-my-presentation-posters",
           title: 'I’m presenting my RA-L paper, and have two workshop papers (Long Horizon Motion...',
           description: "",
-          section: "News",},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
+          section: "News",},{id: "projects-project-8",
           title: 'project 8',
           description: "an other project with a background image and giscus comments",
           section: "Projects",handler: () => {
