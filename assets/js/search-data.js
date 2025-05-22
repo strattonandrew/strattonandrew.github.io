d8512eea99743509ec3d5e9018946f67da1f82c8
strattonandrew <73186396+strattonandrew@users.noreply.github.com>
2025-05-22 05:06:39 +0000

    Deploying to gh-pages from @ strattonandrew/strattonandrew.github.io@e41007c0e4f7edf5deafc3dc906e1fca2f88629c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -417,12 +417,7 @@ ninja.data = [{
           section: "News",},{id: "news-i-m-presenting-my-ra-l-paper-and-have-two-workshop-papers-long-horizon-motion-prediction-and-advances-in-social-navigation-at-icra-2025-come-say-hello-at-my-presentation-posters",
           title: 'I’m presenting my RA-L paper, and have two workshop papers (Long Horizon Motion...',
           description: "",
-          section: "News",},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
+          section: "News",},{id: "projects-project-3-with-very-long-name",
           title: 'project 3 with very long name',
           description: "a project that redirects to another website",
           section: "Projects",handler: () => {
